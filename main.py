f06962c9ba55e549b7808fc7f1b24b833c8aa0f6
Adam Mazur <adammazur445@gmail.com>
2023-08-03 15:37:59 +0200

    Added sign-up page and interests page

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
+
+@app.route('/interests')
+def interests():
+    return render_template("interests.html")
